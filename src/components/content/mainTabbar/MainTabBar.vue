2e3c9883d70320a7668750cd562d2cf7dee076d8
xiangyong <xy040791@163.com>
2020-11-25 20:52:12 +0800

    '初始化项目'

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <TabBar>
+      <TabBarItem path="/home" activeColor="blue">
+        <img slot="item-icon" src="~@/assets/img/tabbar/home.png" alt="">
+        <img slot="item-icon-active" src="~@/assets/img/tabbar/home_active.png" alt="">
+        <div slot="item-text">首页</div>
+      </TabBarItem>
+      <TabBarItem path="/category" activeColor="deepPink">
+        <img slot="item-icon" src="~@/assets/img/tabbar/Category.png" alt="">
+        <img slot="item-icon-active" src="~@/assets/img/tabbar/Category_active.png" alt="">
+        <div slot="item-text">分类</div>
+      </TabBarItem>
+      <TabBarItem path="/shop" activeColor="blue">
+        <img slot="item-icon" src="~@/assets/img/tabbar/shop.png" alt="">
+        <img slot="item-icon-active" src="~@/assets/img/tabbar/shop_active.png" alt="">
+        <div slot="item-text">购物车</div>
+      </TabBarItem>
+      <TabBarItem path="/profile" activeColor="deepPink">
+        <img slot="item-icon" src="~@/assets/img/tabbar/profile.png" alt="">
+        <img slot="item-icon-active" src="~@/assets/img/tabbar/profile_active.png" alt="">
+        <div slot="item-text">我的</div>
+      </TabBarItem>
+    </TabBar>
+</template>
+
+<script>
+  import TabBar from "../../common/tabbar/TabBar";
+  import TabBarItem from "../../common/tabbar/TabBarItem";
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBarItem,
+      TabBar
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
